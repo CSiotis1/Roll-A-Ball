c689a93e5c3b6e6078de6f07551037687799ed28
CSiotis1 <136394296+CSiotis1@users.noreply.github.com>
2023-06-13 09:22:15 +1000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int health = 100;
+    public float speed = 2000.0f;
+    public string playerName = "Goober";
+    public bool isGameOver = false;
+    public Vector3 startingPos;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startingPos;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOver)
+        {
+            health = health - 1;
+            print("Health is " + health);
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+}
